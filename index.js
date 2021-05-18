35e9e0dd3f998e7922b569f1e1f1727e25eab481
keerthana-kattamuri <keerthana.kattamuri@gmail.com>
2021-05-18 12:13:33 +0530

    updated config

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
 import swaggerUI from "swagger-ui-express";
-import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerJsDoc from "swagger-jsdoc";
 import { Routes } from "./Routes/Routes";
 
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
       title: "Appointment Booking API",
-      version: '1.0.0',
+      version: "1.0.0",
     },
-    servers: ["http://localhost:4000"]
+    servers: ["http://localhost:4000"],
   },
-  apis: ['Swagger.js'],
+  apis: ["Swagger.js"],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs, { explorer: true }));
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocs, { explorer: true })
+);
 
 mongoose.Promise = global.Promise;
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // mongodb://localhost:27017/<DatabaseName>
-const uri = "mongodb://localhost:27017/Appointment";
+const uri = isProduction
+  ? process.env.DATABASE_URL
+  : "mongodb://localhost:27017/Appointment";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -38,7 +46,7 @@ db.once("open", () => {
   console.log("error occured", error);
 });
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
